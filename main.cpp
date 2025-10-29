79232e525a9048aec1c67ef385040041b15b7572
Lucas Fernandes <LUCASFERNANDESCS@OUTLOOK.COM>
2025-10-29 14:56:52 -0300

    refactor(main): add try/catch blocks for error handling

--- main.cpp
+++ main.cpp
@@ -1,120 +1,195 @@
 #include <iostream>
-#include "Produto.h"
-#include "EstoqueService.h"
+#include "../include/EstoqueService.h"
+#include "../include/Carrinho.h"
+#include "../include/Exceptions.h"
+
 using namespace std;
 
-int main()
-{
-    EstoqueService estoque;
-    int opcao;
+int main() {
+    EstoqueService estoqueService;
+    Carrinho carrinho;
 
-    do
-    {
-        cout << "\n=== SISTEMA DE ESTOQUE ===\n";
-        cout << "1. Cadastrar produto\n";
-        cout << "2. Listar produtos\n";
-        cout << "3. Adicionar estoque\n";
-        cout << "4. Vender produto\n";
-        cout << "5. Remover produto\n";
-        cout << "6. Alterar preco de produto\n";
-        cout << "7. Alterar ID do produto\n";
+    int option, subOption, caixaOption, id, qtd;
+    string nome;
+    double preco;
+
+    do {
+        cout << "\n=== Menu Principal ===\n";
+        cout << "1. Cadastrar/Alterar/Remover Produtos\n";
+        cout << "2. Abrir Caixa\n";
         cout << "0. Sair\n";
         cout << "Escolha uma opcao: ";
-        cin >> opcao;
+        cin >> option;
+        cin.ignore();
 
-        switch (opcao)
-        {
-        case 1:
-        {
-            int qtd;
-            string nome;
-            double preco;
+        switch (option) {
+            case 1: {
+                do {
+                    cout << "\n=== Cadastrar/Alterar/Remover Produtos ===\n";
+                    cout << "1. Cadastrar Produto\n";
+                    cout << "2. Adicionar Quantidade ao Estoque\n";
+                    cout << "3. Alterar preco do Produto\n";
+                    cout << "4. Remover Produto\n";
+                    cout << "5. Listar Produtos\n";
+                    cout << "0. Voltar ao menu principal\n";
+                    cout << "Escolha: ";
+                    cin >> subOption;
+                    cin.ignore();
 
-            cout << "\n--- Cadastro de Produto ---\n";
-            cin.ignore();
-            cout << "Nome: ";
-            getline(cin, nome);
-            cout << "Preco: ";
-            cin >> preco;
-            cout << "Quantidade: ";
-            cin >> qtd;
+                    switch (subOption) {
+                        case 1: {
+                            try {
+                                cout << "Nome: ";
+                                getline(cin, nome);
+                                cout << "Preco: ";
+                                cin >> preco;
+                                cout << "Quantidade: ";
+                                cin >> qtd;
+                                cin.ignore();
 
-            Produto p(nome, preco, qtd);
-            estoque.cadastrarProduto(p);
-            break;
-        }
-        case 2:
-        {
-            cout << "\n--- Lista de Produtos ---\n";
+                                Produto novoProduto(nome, preco, qtd);
+                                estoqueService.adicionarProduto(novoProduto);
 
-            estoque.listarProdutos();
-            break;
-        }
-        case 3:
-        {
-            int id, qtd;
-            cout << "\nID do produto para adicionar estoque: ";
-            cin >> id;
-            cout << "Quantidade a adicionar: ";
-            cin >> qtd;
+                                cout << "Produto cadastrado com sucesso!" << endl;
+                            } catch (Exceptions& e) {
+                                cout << "Erro: " << e.what() << endl;
+                            }
+                            break;
+                        }
 
-            estoque.adicionarEstoque(id, qtd);
-            break;
-        }
-        case 4:
-        {
-            int id, qtd;
-            cout << "\nID do produto para vender: ";
-            cin >> id;
-            cout << "Quantidade a vender: ";
-            cin >> qtd;
+                        case 2: {
+                            try {
+                                cout << "ID: ";
+                                cin >> id;
+                                cout << "Quantidade: ";
+                                cin >> qtd;
+                                cin.ignore();
 
-            estoque.venderProduto(id, qtd);
-            break;
-        }
-        case 5:
-        {
-            int id;
-            cout << "ID do produto para remover: ";
-            cin >> id;
+                                estoqueService.adicionarEstoque(id, qtd);
+                            } catch (Exceptions& e) {
+                                cout << "Erro: " << e.what() << endl;
+                            }
+                            break;
+                        }
 
-            estoque.removerProduto(id);
-            break;
-        }
-        case 6: {
-            int id;
-            double preco;
-            cout << "\nID do produto: ";
-            cin >> id;
-            cout << "Novo preco: ";
-            cin >> preco;
+                        case 3: {
+                            try {
+                                cout << "ID: ";
+                                cin >> id;
+                                cout << "Novo preco: ";
+                                cin >> preco;
+                                cin.ignore();
 
-            estoque.alterarPreco(id, preco);
-            break;
-        }
-        case 7: {
-            int id;
-            int novoId;
-            cout << "\nID do produto que deseja alterar: ";
-            cin >> id;
-            cout << "Novo ID do produto: ";
-            cin >> novoId;
+                                estoqueService.alterarPreco(id, preco);
+                            } catch (Exceptions& e) {
+                                cout << "Erro: " << e.what() << endl;
+                            }
+                            break;
+                        }
 
-            estoque.alterarIDProduto(id, novoId);
-            break;
-        }
-        case 0:
-        {
-            break;
-        }
-        default:
-        {
-            cout << "\nOpcao Invalida! Tente novamente.\n";
-            break;
-        }
-        }
+                        case 4: {
+                            try {
+                                cout << "ID: ";
+                                cin >> id;
+                                cin.ignore();
 
-    } while (opcao != 0);
-    cout << "Encerrando programa...\n";
+                                estoqueService.removerProduto(id);
+                            } catch (Exceptions& e) {
+                                cout << "Erro: " << e.what() << endl;
+                            }
+                            break;
+                        }
+
+                        case 5:
+                            try {
+                                estoqueService.listarProdutos();
+                            } catch (const Exceptions& e) {
+                                cout << e.what() << endl;
+                            }
+                            break;
+
+                        case 0:
+                            break;
+
+                        default:
+                            cout << "Opcao invalida!\n";
+                    }
+                } while (subOption != 0);
+                break;
+            }
+
+            case 2: {
+                do {
+                    cout << "\n=== Abrir Caixa ===\n";
+                    cout << "1. Adicionar Produto ao Carrinho\n";
+                    cout << "2. Remover Produto do Carrinho\n";
+                    cout << "3. Listar Produtos no Carrinho\n";
+                    cout << "4. Finalizar Compra\n";
+                    cout << "0. Voltar ao menu principal\n";
+                    cout << "Escolha: ";
+                    cin >> caixaOption;
+                    cin.ignore();
+
+                    switch (caixaOption) {
+                        case 1: {
+                            try {
+                                cout << "ID do produto: ";
+                                cin >> id;
+                                cout << "Quantidade: ";
+                                cin >> qtd;
+                                cin.ignore();
+
+                                carrinho.adicionarProdutoAoCarrinho(estoqueService, id, qtd);
+                            } catch (Exceptions& e) {
+                                cout << "Erro: " << e.what() << endl;
+                            }
+                            break;
+                        }
+
+                        case 2: {
+                            try {
+                                cout << "ID do produto a remover: ";
+                                cin >> id;
+                                cin.ignore();
+
+                                carrinho.removerItem(id);
+                            } catch (Exceptions& e) {
+                                cout << "Erro: " << e.what() << endl;
+                            }
+                            break;
+                        }
+
+                        case 3:
+                            carrinho.listarItens();
+                            break;
+
+                        case 4: {
+                            try {
+                                carrinho.finalizarCompra(estoqueService);
+                            } catch (Exceptions& e) {
+                                cout << "Erro ao finalizar compra: " << e.what() << endl;
+                            }
+                            break;
+                        }
+
+                        case 0:
+                            break;
+
+                        default:
+                            cout << "Opcao invalida!\n";
+                    }
+                } while (caixaOption != 0);
+                break;
+            }
+
+            case 0:
+                cout << "Saindo do programa...\n";
+                break;
+
+            default:
+                cout << "Opcao invalida! Tente novamente.\n";
+        }
+    } while (option != 0);
+
     return 0;
 }
